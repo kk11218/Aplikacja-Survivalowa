f3dc862980e51f6328abe6043981e2fd6499f2e8
Boguslaw0 <116185852+Boguslaw0@users.noreply.github.com>
2022-11-12 17:14:11 +0100

    Latarka

--- app/src/main/java/com/example/surwiwalapk/MainActivity.java
+++ app/src/main/java/com/example/surwiwalapk/MainActivity.java
@@ -2,13 +2,74 @@ package com.example.surwiwalapk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton toggleButton;
+
+    boolean hasCameraFlash = false;
+    boolean flashOn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toggleButton = findViewById(R.id.imageButton);
+
+        hasCameraFlash = getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              if (hasCameraFlash){
+                  if (flashOn){
+                      flashOn = false;
+                      toggleButton.setImageResource(R.drawable.off);
+                      try {
+                          flashLightOff();
+                      } catch (CameraAccessException e) {
+                          e.printStackTrace();
+                      }
+                  }
+                  else{
+                      flashOn = true;
+                      toggleButton.setImageResource(R.drawable.on);
+                      try {
+                          flashLightOn();
+                      } catch (CameraAccessException e) {
+                          e.printStackTrace();
+                      }
+                  }
+              }
+              else{
+                  Toast.makeText(MainActivity.this, "No flash available on your device", Toast.LENGTH_LONG).show();
+              }
+            }
+        });
     }
+
+    private void flashLightOn() throws CameraAccessException {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        assert cameraManager != null;
+        String cameraId = cameraManager.getCameraIdList()[0];
+        cameraManager.setTorchMode(cameraId, true);
+        Toast.makeText(MainActivity.this, "FlashLight is ON", Toast.LENGTH_SHORT).show();
+    }
+
+    private void flashLightOff() throws CameraAccessException {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        assert cameraManager != null;
+        String cameraId = cameraManager.getCameraIdList()[0];
+        cameraManager.setTorchMode(cameraId, false);
+        Toast.makeText(MainActivity.this, "FlashLight is OFF", Toast.LENGTH_SHORT).show();
+    }
+
 }
