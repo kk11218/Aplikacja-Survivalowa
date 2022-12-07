361e3304c89fd34f5c7d3093631f788d8e008581
Boguslaw0 <116185852+Boguslaw0@users.noreply.github.com>
2022-12-07 14:40:36 +0100

    tajmer0.2

--- app/src/main/java/com/example/surwiwalapk/Tajmer.java
+++ app/src/main/java/com/example/surwiwalapk/Tajmer.java
@@ -2,13 +2,47 @@ package com.example.surwiwalapk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+import java.util.Locale;
 
 public class Tajmer extends AppCompatActivity {
+
+    Button timeButton;
+    int hour, minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tajmer);
+        timeButton = findViewById(R.id.timeButton);
+
+
+    }
+
+
+    public void popTimePicker(View view)
+    {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker,int selectedHour,int selectedMinute)
+            {
+                hour = selectedHour;
+                minute = selectedMinute;
+                timeButton.setText(String.format(Locale.getDefault(), "%02d:%02d",hour ,minute));
+            }
+        };
+
+        int style = AlertDialog.THEME_HOLO_DARK;
+
+        TimePickerDialog timePickerDialog =new TimePickerDialog(this,style, onTimeSetListener,hour,minute,true);
+
+        timePickerDialog.setTitle("00:00");
+        timePickerDialog.show();
     }
 }
